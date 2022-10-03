0e4cca1713e25ba35d8ede29c41d7e46dce8ac26
Kai Patel <00kaipatel@gmail.com>
2022-10-03 14:28:09 +0100

    Commenting

--- Main.hs
+++ Main.hs
@@ -6,16 +6,18 @@ data Term = Variable Var | Lambda Var Term | Apply Term Term deriving (Show)
 alphaEq :: Term -> Bool
 alphaEq = undefined
 
+-- Find all free variables in a given expression
 free :: Term -> [Var]
 free (Variable v) = [v]
 free (Lambda x m) = filter (\i -> i /= x) (free m)
 free (Apply m n) = free(m) ++ free(n)
 
+-- Get a fresh variable i.e. the first variable not in the given list
 fresh :: [Var] -> Var
 fresh xs = head $ dropWhile (\x -> x `elem` xs) variables
     where variables = [l : [] | l <- ['a' .. 'z']] ++ [l : show x | x <- [1 ..], l <- ['a' .. 'z']]
 
---- Substitution with explicit alpha conversion
+-- Substitution with explicit alpha conversion
 substitute :: Term -> Var -> Term -> Term
 substitute (Variable y) x (n)
     | y == x = n
